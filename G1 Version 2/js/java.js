f44ed2017cd54d956c6440dc21be4cb7babe76a3
lpacyk <lpacyk31@gmail.com>
2014-03-16 23:25:19 -0700

    Bodywrapper>bodycontent

--- G1 Version 2/js/java.js
+++ G1 Version 2/js/java.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
 var Height = $('#footer').height() + $('#bodyContent').height() + $('#header').height();
 var newHeight = ($(window).height() - Height) + ($('#bodyWrapper').height());
     if(Height < $(window).height()) {
-            $('#bodyWrapper').css('height', newHeight  + 'px');
+            $('#bodyContent').css('height', newHeight  + 'px');
 }
 });
