6185b2743cf9b802d7b54b8154caa0a0e873475f
lpacyk <lpacyk31@gmail.com>
2014-03-16 23:23:59 -0700

    Fixed Javascript

--- G1 Version 2/js/java.js
+++ G1 Version 2/js/java.js
@@ -1,22 +1,8 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-var winHeight = $(window).height();
-var footerHeight = $('#footer').height();
-var headerHeight = $('#header').height();
-var contentHeight = $('#bodyContent').height();
-var docHeight = (footerHeight + headerHeight + contentHeight);
-var Height = (winHeight - footerHeight - headerHeight);
 
-if  (docHeight < winHeight){
-	$('#bodyWrapper').css('height', Height + 1 + 'px');
-	}
-
-});
-=======
 var Height = $('#footer').height() + $('#bodyContent').height() + $('#header').height();
 var newHeight = ($(window).height() - Height) + ($('#bodyWrapper').height());
     if(Height < $(window).height()) {
             $('#bodyWrapper').css('height', newHeight  + 'px');
 }
 });
->>>>>>> origin/Leon-Ho
