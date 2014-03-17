7ff045d45980f5ee0dcd75f19305e04b2a064aa8
lpacyk <lpacyk31@gmail.com>
2014-03-16 23:26:52 -0700

    fixed

--- G1 Version 2/js/java.js
+++ G1 Version 2/js/java.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-
 var Height = $('#footer').height() + $('#bodyContent').height() + $('#header').height();
-var newHeight = ($(window).height() - Height) + ($('#bodyWrapper').height());
+var newHeight = ($(window).height() - Height) + ($('#bodyContent').height());
     if(Height < $(window).height()) {
             $('#bodyContent').css('height', newHeight  + 'px');
 }
