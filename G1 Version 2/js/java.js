c9c3f36bedadc75b373d73152f6313cdb415ff32
lpacyk <lpacyk31@gmail.com>
2014-03-16 21:40:12 -0700

    Footer Script
    Adjusted footer script

--- G1 Version 2/js/java.js
+++ G1 Version 2/js/java.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 var docHeight = $(window).height();
 var footerHeight = $('#footer').height();
 var headerHeight = $('#header').height();
-var contentHeight = $('#bodyWrapper').height()
+var contentHeight = $('#bodyContent').height()
 var footerTop = $('#footer').position().top + footerHeight;
 var Height = (docHeight - footerHeight - headerHeight);
 
 if  (contentHeight < Height){
-$('#bodyWrapper').css('height', Height  + 'px');
+$('#bodyContent').css('height', Height  + 'px');
 }
 
 });
