8aa3769240455d1345965de98fc62f2edcf7db15
lpacyk <lpacyk31@gmail.com>
2014-03-16 22:10:26 -0700

    Footer
    Test

--- G1 Version 2/js/java.js
+++ G1 Version 2/js/java.js
@@ -3,12 +3,11 @@ $(document).ready(function() {
 var docHeight = $(window).height();
 var footerHeight = $('#footer').height();
 var headerHeight = $('#header').height();
-var contentHeight = $('#bodyContent').height()
-var footerTop = $('#footer').position().top + footerHeight;
+var contentHeight = $('#bodyContent').height();
 var Height = (docHeight - footerHeight - headerHeight);
 
 if  (contentHeight < Height){
-$('#bodyContent').css('height', Height  + 'px');
+$('#bodyWrapper').css('height', Height  + 'px');
 }
 
 });
