a1d80ab7957c07e4063df6b1c89053874bdc893a
lpacyk <lpacyk31@gmail.com>
2014-03-16 23:17:47 -0700

    Roll Back
    Errors

--- G1 Version 2/js/java.js
+++ G1 Version 2/js/java.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-
-var docHeight = $(window).height();
+var winHeight = $(window).height();
 var footerHeight = $('#footer').height();
 var headerHeight = $('#header').height();
-var contentHeight = $('#bodyContent').height()
-var footerTop = $('#footer').position().top + footerHeight;
-var Height = (docHeight - footerHeight - headerHeight);
+var contentHeight = $('#bodyContent').height();
+var docHeight = (footerHeight + headerHeight + contentHeight);
+var Height = (winHeight - footerHeight - headerHeight);
 
-if  (contentHeight < Height){
-$('#bodyContent').css('height', Height  + 'px');
-}
+if  (docHeight < winHeight){
+	$('#bodyWrapper').css('height', Height + 1 + 'px');
+	}
 
 });
