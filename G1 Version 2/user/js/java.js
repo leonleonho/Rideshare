4c253923637854bb08150c6876096fcb8f9ca83f
Leon Ho <leon.lai.ho@gmail.com>
2014-03-16 23:16:41 -0700

    Fixed sticky footer

--- G1 Version 2/user/js/java.js
+++ G1 Version 2/user/js/java.js
@@ -1,14 +1,7 @@
+
 $(document).ready(function() {
-
-var docHeight = $(window).height();
-var footerHeight = $('#footer').height();
-var headerHeight = $('#header').height();
-var contentHeight = $('#bodyWrapper').height()
-var footerTop = $('#footer').position().top + footerHeight;
-var Height = (docHeight - footerHeight - headerHeight);
-
-if  (contentHeight < Height){
-$('#bodyWrapper').css('height', Height  + 'px');
+var Height = $('#footer').height() + $('#bodyContent').height() + $('#header').height();
+    if(Height < $(window).height()) {
+            alert("Too short");
 }
-
 });
